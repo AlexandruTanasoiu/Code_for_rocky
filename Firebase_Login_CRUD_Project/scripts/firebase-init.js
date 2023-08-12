bf92521b760829ab8691c1560603e4b3877518c5
Alexandru Tanasoiu <60856356+AlexandruTanasoiu@users.noreply.github.com>
2023-08-12 18:00:17 +0300

    SignUp page is done

--- Firebase_Login_CRUD_Project/scripts/firebase-init.js
+++ Firebase_Login_CRUD_Project/scripts/firebase-init.js
@@ -1,27 +1,123 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
-import { getAuth } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js';
-import { getDatabase } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js';
-// import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
+import {
+  getDatabase,
+  ref,
+  set,
+  child,
+  get,
+  onValue,
+  remove,
+} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configurations
 const firebaseConfig = {
-    apiKey: "AIzaSyBez6BmymCOil4cZggw33AT_3WXcmj9QMc",
-    authDomain: "fir-project-55d21.firebaseapp.com",
-    databaseURL: "https://fir-project-55d21-default-rtdb.firebaseio.com",
-    projectId: "fir-project-55d21",
-    storageBucket: "fir-project-55d21.appspot.com",
-    messagingSenderId: "599313505187",
-    appId: "1:599313505187:web:bc03cf129c32a800f25816",
-    measurementId: "G-GEF6BMSXJ5"
-  };
-// Initialize Firebase
-const firebaseInit = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-const auth = getAuth(firebaseInit);
-const db = getDatabase(firebaseInit);
+  apiKey: "AIzaSyBez6BmymCOil4cZggw33AT_3WXcmj9QMc",
+  authDomain: "fir-project-55d21.firebaseapp.com",
+  databaseURL: "https://fir-project-55d21-default-rtdb.firebaseio.com",
+  projectId: "fir-project-55d21",
+  storageBucket: "fir-project-55d21.appspot.com",
+  messagingSenderId: "599313505187",
+  appId: "1:599313505187:web:bc03cf129c32a800f25816",
+  measurementId: "G-GEF6BMSXJ5",
+};
 
+// Initialize Firebase SDK
+const firebaseInit = initializeApp(firebaseConfig);
+const authService = getAuth(firebaseInit);
+const dbService = getDatabase(firebaseInit);
+
+// declare document form elements
+const firstNameElement = document.getElementById("fnameInput");
+const lastNameElement = document.getElementById("lnameInput");
+const emailUserElement = document.getElementById("emailInput");
+const userNameElement = document.getElementById("usernameInput");
+const passUserElement = document.getElementById("passwordInput");
+const confirmPassUserElement = document.getElementById("confirmPassword");
+const languageSelectionElement = document.querySelectorAll(
+  ".checkbox__container-input"
+);
+const signUpBtnElement = document.getElementById("signUpBtn");
+
+//helper functions
+
+function createNewUser(userEmail, userPass) {
+  createUserWithEmailAndPassword(authService, userEmail, userPass)
+    .then((createdUser) => {
+      console.log("User created!");
+    })
+    .catch((error) =>
+      console.log(
+        `error triggered \n error code: ${error.code} \n error message: ${error.message}`
+      )
+    );
+}
+
+function pushUserData(
+  userId,
+  firstName,
+  lastName,
+  emailUser,
+  userName,
+  passUser,
+  languageList
+) {
+  const userObject = {
+    fname: firstName,
+    lname: lastName,
+    email: emailUser,
+    username: userName,
+    pass: passUser,
+    skills: languageList,
+  };
+  set(ref(dbService, `users/${userId}`), userObject);
+}
+
+function getDataForm() {
+  const firstName = firstNameElement.value;
+  const lastName = lastNameElement.value;
+  const emailUser = emailUserElement.value;
+  const userName = userNameElement.value;
+  const passUser = passUserElement.value;
+  const confirmPassUser = confirmPassUserElement.value;
+  const languageList = [];
+  languageSelectionElement.forEach((input) =>
+    input.checked ? languageList.push(input.value) : ""
+  );
+
+  if (emailUser && userName && passUser && confirmPassUser) {
+    if (passUser === confirmPassUser) {
+      createNewUser(emailUser, passUser);
+      // assign a name to account
+      onAuthStateChanged(authService, (signedUser) => {
+        if (signedUser) {
+          signedUser.displayName = userName;
+          const userId = signedUser.uid;
+          pushUserData(
+            userId,
+            firstName,
+            lastName,
+            emailUser,
+            userName,
+            passUser,
+            languageList
+          );
+          console.log("Display name and user data stored!");
+        } else console.log("User is signed out!");
+      });
+    } else alert("Password didn't match!");
+    signOut(authService);
+  } else alert("Fields cannot be empty!");
+}
+
+signUpBtnElement.addEventListener("click", (event) => {
+  event.preventDefault();
+  getDataForm();
+});
